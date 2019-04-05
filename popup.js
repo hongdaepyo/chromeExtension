33a81dc392d08b8c792618118223f39b737b3741
dphong <dphong@kaoni.com>
2019-04-05 18:06:48 +0900

    리스트 클릭 시, 삭제하는 로직 구현중

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
 let changeColor = document.getElementById('changeColor');
 
+document.body.addEventListener("click", function(e){
+	if(e.target.tagName.toLowerCase() === "li") {
+		alert('li');
+	}
+});
+
 chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
@@ -8,6 +14,7 @@ chrome.storage.sync.get('color', function(data) {
 changeColor.onclick = function(element) {
 	let color = element.target.value;
 	let testStr = "";
+	let popupMainDiv = document.getElementById("popupMainDiv");
 	// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		// chrome.tabs.executeScript(
 			// tabs[0].id,
@@ -18,12 +25,41 @@ changeColor.onclick = function(element) {
 			// function(tab){document.body.innerHTML = tabs.length}
 		// );
 	// });
+	
 
 	chrome.tabs.query({}, function(tabs) {
+		testStr += "<ul id='tabListUL'>";
 		for(var i = 0; tabs[i]; i++){
-			testStr += (i + 1) + ". " + tabs[i].id + " : " + tabs[i].url + "<br>"
+			testStr += "<li id='tabListLi_" + i + "' name='" + tabs[i].id + "'>" 
+						+ (i + 1) + ". " + tabs[i].id + " : " + tabs[i].url + "</li>";
 		}
-		document.body.innerHTML = testStr;
+		testStr += "</ul>"
+		popupMainDiv.innerHTML = testStr;
+		
+		// chrome.tabs.get(122, function(tab){
+			// chrome.tabs.remove(tab.id);
+		// });
+		
+		// addTabListEvent();
+		
+		//디버깅용
+		document.body.innerText += document.body.innerHTML;
 	});
+		
+}
+
+function addTabListEvent(){
 	
+	var tabList = document.querySelectorAll("#tabListUL li");
+	[].forEach.call(tabList, function(tab){
+		tab.addEventListener("click", tabClose);
+	});
+}
+
+function tabClose(e){
+	alert(e.target.name);
+	var tabID = e.target.name.match(/\d+/g)[0];
+	chrome.tabs.get(tabID, function(tab){
+		chrome.tabs.remove(tab.id);
+	});
 }
