ce4610684c813bdc4292e4242d7902405295ee44
dphong <dphong@kaoni.com>
2019-04-08 14:49:22 +0900

    스크립트 오류 수정.

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ chrome.runtime.onInstalled.addListener(function() {
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 	  chrome.declarativeContent.onPageChanged.addRules([{
-		  conditions: [new chrome.declarativeContent.pageStateMatcher({
+		  conditions: [new chrome.declarativeContent.PageStateMatcher({
 			pageUrl: {hostEquals: 'developer.chrome.com'}  
 		  })
 		  ],
